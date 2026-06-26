74988121f2d17f9248ad70b5aad9579c09bb9a2b
Aniruth Venkedesh <aven50021@gmail.com>
2026-06-26 18:36:36 +0530

    utils.py + class Pathway definition

---
+++ src/model.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class Pathway:
+    """
+    Definines pathways characteristics
+    """
+    def __init__(self, tau_T, tau_E, beta):
+        self.T = 0.0
+        self.E = 0.0
+        self.L = 0.0
+        self.tau_T = tau_T
+        self.tau_E = tau_E
+        self.beta = beta
+
+    def stimulatie(self, s, e):
+        self.T += s
+        self.E += e
+
+    def derivatives(self, P):
+        return [-self.T/self.tau_T, -self.E/self.tau_E, self.beat*self.T*P]
