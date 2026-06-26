720f1f47ff29a8e1f53ca315b1f28fd7131f63a5
Aniruth Venkedesh <aven50021@gmail.com>
2026-06-26 22:00:53 +0530

    added Pool class

--- src/model.py
+++ src/model.py
@@ -1,9 +1,6 @@
 import numpy as np
 
 class Pathway:
-    """
-    Definines pathways characteristics
-    """
     def __init__(self, tau_T, tau_E, beta):
         self.T = 0.0
         self.E = 0.0
@@ -17,4 +14,15 @@ class Pathway:
         self.E += e
 
     def derivatives(self, P):
-        return np.array([-self.T/self.tau_T, -self.E/self.tau_E, self.beat*self.T*P])
+        return np.array([-self.T/self.tau_T, -self.E/self.tau_E, self.beta*self.T*P])
+
+class Pool:
+    def __init__(self, tau_p):
+        self.P = 0.0
+        self.tau_p = tau_p
+    
+    def derivatives(self):
+        return -self.P/self.tau_p
+    
+    def add_product(self, P):
+        self.P += P
